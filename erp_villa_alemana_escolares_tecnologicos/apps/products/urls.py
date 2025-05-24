5eac420ee0c5102b4a6bb9e21585a9c6221db3a9
Diego Rivera Estefano <diego.rivera.estefano@gmail.com>
2025-05-24 00:11:29 -0400

    url change for use slug

--- erp_villa_alemana_escolares_tecnologicos/apps/products/urls.py
+++ erp_villa_alemana_escolares_tecnologicos/apps/products/urls.py
@@ -15,9 +15,21 @@ app_name = "products"
 urlpatterns = [
     path("product/", view=product_list_view, name="product-list"),
     path("product/create/", view=product_create_view, name="product-create"),
-    path("product/delete/<int:pk>", view=product_delete_view, name="product-delete"),
-    path("product/update/<int:pk>", view=product_update_view, name="product-update"),
-    path("product/detail/<int:pk>/", view=product_detail_view, name="product-detail"),
+    path(
+        "product/delete/<str:product_slug>",
+        view=product_delete_view,
+        name="product-delete",
+    ),
+    path(
+        "product/update/<str:product_slug>",
+        view=product_update_view,
+        name="product-update",
+    ),
+    path(
+        "product/detail/<str:product_slug>/",
+        view=product_detail_view,
+        name="product-detail",
+    ),
     path(
         "category/",
         view=product_category_list_view,
@@ -29,17 +41,17 @@ urlpatterns = [
         name="category-create",
     ),
     path(
-        "category/delete/<str:pk>",
+        "category/delete/<str:category_slug>",
         view=product_category_delete_view,
         name="category-delete",
     ),
     path(
-        "category/update/<str:pk>",
+        "category/update/<str:category_slug>",
         view=product_category_update_view,
         name="category-update",
     ),
     path(
-        "category/detail/<str:pk>/",
+        "category/detail/<str:category_slug>/",
         view=product_category_detail_view,
         name="category-detail",
     ),
